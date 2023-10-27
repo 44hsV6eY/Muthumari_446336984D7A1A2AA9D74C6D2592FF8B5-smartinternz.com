0b8b0aaa378ff90663f60e36317b3221d2ad9596
44hsV6eY <145115374+44hsV6eY@users.noreply.github.com>
2023-10-27 21:43:52 +0530

    Add files via upload

---
+++ productlist.py
@@ -0,0 +1,12 @@
+def lstProduct(myLst):
+
+    result = 1
+    for x in myLst:
+        result = result * x
+    return result
+
+
+lst1 = [10, 20, 30]
+lst2 = [4, 5, 6]
+print(lstProduct(lst1))
+print(lstProduct(lst2))
